0b6563e1f9d27c699df8305ee2e9f27f15ce28ee
nlweeks <noah.weeks@gmail.com>
2025-03-21 12:12:32 -0400

    Initial Commit

---
+++ ToMe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToMe
+//
+//  Created by Noah Weeks on 3/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToMe/ToMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToMeApp.swift
+//  ToMe
+//
+//  Created by Noah Weeks on 3/21/25.
+//
+
+import SwiftUI
+
+@main
+struct ToMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
